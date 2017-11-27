09bca435604d2d41c8b6e2eb7f1ed65547cbf532
lexis25 <tokallex@gmail.com>
2017-11-27 11:34:47 +0200

    imageLoader

---
+++ TetroBox/src/main/java/graphics/Loader.java
@@ -0,0 +1,22 @@
+package graphics;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Loader {
+
+    private static final String PATH = "/res";
+
+    public static BufferedImage loadImage(String fileName){
+        BufferedImage image = null;
+        try{
+            image = ImageIO.read(new File(PATH + fileName));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+}
